0dfb411f9eed656a1e3bde636f4476930988359e
shebah-2017 <shebah-2017@users.noreply.github.com>
2017-05-17 02:41:24 -0700

    Add files via upload

---
+++ progamming work.py
@@ -0,0 +1,10 @@
+
+import datetime
+years =float(input('Enter your age:'))
+month=int(input('Enter month of birth:'))
+day=int(input('Date of birth:'))
+days_per_year=365.24
+birthdate =datetime.datetime.now()  - datetime.timedelta(days=(years*days_per_year))
+y=int(birthdate.strftime("%Y"))
+x=datetime.date(y,month,day)
+print(x.strftime("You were born on %A, %d-%B-%Y"))
